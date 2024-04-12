c6d23453b8660690c609e8c3754d3421199fbe6c
Jimii <jimmyimpulse2@gmail.com>
2024-04-12 21:14:27 +0300

    feat: mint core asset

--- tests/utils/constants.ts
+++ tests/utils/constants.ts
@@ -2,7 +2,7 @@ import { AnchorProvider, Provider } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 
 // default provider provided by Anchor.toml
-export const defaultProvider: Provider = new anchor.AnchorProvider(
+export const defaultProvider: Provider = new AnchorProvider(
 	AnchorProvider.env().connection,
 	AnchorProvider.env().wallet,
 	AnchorProvider.defaultOptions()
@@ -16,4 +16,9 @@ export const SOUNDWORK_LIST = new PublicKey(
 );
 export const SOUNDWORK_CREATE = new PublicKey(
 	"8gg4YauYXorr3YKUgZvmti61wWHDmhRfYqQZZYUnrHuc"
+);
+
+// external programs
+export const CORE_PROGRAM_ID = new PublicKey(
+	"CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
 );
--- tests/utils/programs.ts
+++ tests/utils/programs.ts
@@ -15,31 +15,46 @@ import {
 import { loadProgramIdl } from "./helpers";
 
 export class BidProgram {
-	constructor(readonly provider: Provider = defaultProvider) {
-		new Program(
-			loadProgramIdl("bid"),
-			SOUNDWORK_BID,
-			provider
+	constructor(readonly provider: Provider = defaultProvider) {}
+
+	getProgram(): Program<SoundworkBid> {
+		const idl = loadProgramIdl("bid");
+		const programId = SOUNDWORK_BID;
+
+		return new Program(
+			idl,
+			programId,
+			this.provider
 		) as Program<SoundworkBid>;
 	}
 }
 
 export class CreateProgram {
-	constructor(readonly provider: Provider = defaultProvider) {
-		new Program(
-			loadProgramIdl("create"),
-			SOUNDWORK_CREATE,
-			provider
+	constructor(readonly provider: Provider = defaultProvider) {}
+
+	getProgram(): Program<SoundworkCreate> {
+		const idl = loadProgramIdl("create");
+		const programId = SOUNDWORK_CREATE;
+
+		return new Program(
+			idl,
+			programId,
+			this.provider
 		) as Program<SoundworkCreate>;
 	}
 }
 
 export class ListProgram {
-	constructor(readonly provider: Provider = defaultProvider) {
-		new Program(
-			loadProgramIdl("list"),
-			SOUNDWORK_LIST,
-			provider
+	constructor(readonly provider: Provider = defaultProvider) {}
+
+	getProgram(): Program<SoundworkList> {
+		const idl = loadProgramIdl("list");
+		const programId = SOUNDWORK_LIST;
+
+		return new Program(
+			idl,
+			programId,
+			this.provider
 		) as Program<SoundworkList>;
 	}
 }
